1ef6bb3a6d8a5f57ef361500fcb926ea64b0a0c6
Veerpartap Singh <veerpartapp02@gmail.com>
2026-06-23 19:29:48 -0700

    shifting to docFlow project, fastapi, pandas,venv

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+
+# panada  df.head-> the head 
+          #df.tail-> the last cell 
+          # df info!
+          #  df.describe
+          # df.columns
+          #df. index
+import pandas as pd
+from io import BytesIO
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def check_engine():
-    return {"message": "ShieldBox Engine is Online!"}
+    return {"message": "DocuFlow Engine is Online!"}
+
+@app.post("/api/split")
+async def split_excel(file: UploadFile = File(...)):
+    contents = await file.read()
+    df = pd.read_excel(BytesIO(contents))
+    employee_count = df['Business Unit'].nunique()
+    return {"message": f"Found {employee_count} employees in the sheet"}
